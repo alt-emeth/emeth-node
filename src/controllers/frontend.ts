3a4cb663f12fed0f1811f30f84ff7ff4c04a2cca
anhnhicts <anhnh@icts.vn>
2020-08-25 10:46:51 +0700

    update pagination

--- src/controllers/frontend.ts
+++ src/controllers/frontend.ts
@@ -19,12 +19,17 @@ export const dashboard = async (req: Request, res: Response) => {
       title: 'Antiblocks | Dashboard',
       transactions,
       blocks,
+      pages: [1, 2, 3],
+      currentPage: 2,
+      totalPage: 5,
     });
   } catch (error) {
     res.render('dashboard', {
       title: 'Antiblocks | Dashboard',
       transactions,
       blocks,
+      pages: [],
+      totalPage: 0,
     });
   }
 };
--- src/services/dashboard.service.ts
+++ src/services/dashboard.service.ts
@@ -1,5 +1,13 @@
 import Axios from 'axios';
 
 export const getDashboard = async () => {
-  return await Axios.get(process.env.ANTI_BLOCK_API_URL);
+  const url = process.env.ANTI_BLOCK_API_URL;
+  if (!url) {
+    throw new Error('URL does not exist!');
+  }
+  const dataRes = await Axios.get(url);
+  if (!dataRes) {
+    throw new Error('Respone data error');
+  }
+  return dataRes;
 };
