3434e4d923b50a2814da76633372fd5c80194d06
Cuong Nguyen <cuongnv@icts.vn>
2021-07-21 10:32:39 +0700

    get tokenData from scan API

--- src/controllers/frontend.ts
+++ src/controllers/frontend.ts
@@ -23,8 +23,6 @@ import { TOKEN_SPECIAL, SELECT_LIMIT } from '../config/index';
 import { addressPrefix } from '../app';
 import { Erc20Tokens, Erc20TokensDtoCreate } from '../entities/erc20-tokens.entity';
 
-const ERC20_TRANSFER_TOPIC = '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef';
-
 export const index = async (req: Request, res: Response) => {
   res.redirect('/dashboard');
 };
@@ -259,16 +257,16 @@ export const getTransaction = async (req: Request, res: Response) => {
     const { transactionData } = await getTransactionDetail(key);
     const { txEvents } = await getLogsTxs(key);
 
-    const { txTo } = transactionData;
+    const { txTo, transferredTokens } = transactionData;
     const erc20TokensEntity = new Erc20Tokens();
     const isTxToInErc20Tokens = await erc20TokensEntity.findByAddress(txTo);
 
-    for (const txEvent of txEvents) {
-      const { topic0, address: txEventAddress, tokenName, symbol, decimals } = txEvent;
-      if (topic0 === ERC20_TRANSFER_TOPIC && !isTxToInErc20Tokens) {
+    if (!isTxToInErc20Tokens) {
+      for (const transferredToken of transferredTokens) {
+        const { tokenAddress, tokenName, symbol, decimals } = transferredToken;
         try {
           const dataErc20Tokens: Erc20TokensDtoCreate = {
-            address: txEventAddress,
+            address: tokenAddress,
             name: tokenName,
             symbol,
             decimals,
@@ -280,6 +278,7 @@ export const getTransaction = async (req: Request, res: Response) => {
         }
       }
     }
+
     Object.assign(transactionData);
 
     res.render('page-detail/transaction', {
