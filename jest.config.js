7a770ee05e5f6346094d60ad1879ebacde38248b
Toru Nayuki <nayuki@passthetest.co.jp>
2023-07-13 13:15:30 +0900

    Add geth container for tests

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,5 @@
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'ethereum',
-  testEnvironmentOptions: {
-    injectWeb3Provider: true
-  },
-  testPathIgnorePatterns: ['/node_modules/', '<rootDir>/src/eth-lib/']
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src']
 }
